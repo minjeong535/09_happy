778d708b22b9ad3b24117d0331c23b0230d04642
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-10-31 15:35:34 +0900

    0903

--- main.c
+++ main.c
@@ -4,17 +4,17 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	
-	int stu[5];
-	int i;
-	
-	stu[0]=10;
-	stu[1]=20;
-	stu[2]=30;
-	stu[3]=40;
-	stu[4]=50;
-	
-	for (i=0; i<5; i++)
-	 printf("stu[%i]=%i\n", i, stu[i]);
+
+int i;
+int a[5] ={1, 2, 3, 4, 5};
+int b[5] ={1, 2, 3, 4, 5};
+
+for (i=0;i<5; i++)
+{
+	if (a[i] != b[i])
+	{
+		printf("%i is different\n", i);
+	}
+}
 	return 0;
 }
