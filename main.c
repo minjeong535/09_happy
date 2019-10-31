fac6e4988af8ee0baffd1fab191d3498594c9ff4
minjeong535 <55127421+minjeong535@users.noreply.github.com>
2019-10-31 16:21:28 +0900

    0906

--- main.c
+++ main.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define ROWS 3
+#define COLS 3
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+void printMatrix(int a[][COLS]) {
 
-int i;
-int a[5] ={1, 2, 3, 4, 5};
-int b[5] ={1, 2, 3, 4, 5};
-
-for (i=0;i<5; i++)
-{
-	if (a[i] != b[i])
+	int i, j;
+	for (i=0; i<ROWS;i++)
 	{
-		printf("%i is different\n", i);
+		for (j=0; j<COLS; j++)
+		     printf("%i ", a[i][j]);
+	    printf("\n");
 	}
 }
+void addMatrix(int a[][COLS], int b[][COLS], int c[][COLS]){
+	int i, j;
+	for (i=0; i<ROWS; i++)
+	    for(j=0; j<COLS; j++)
+	       c[i][j]= a[i][j] + b[i][j];
+}
+
+
+int main(void)
+{
+	//더하기 대상 행렬(1)
+	int A[ROWS][COLS]=
+	{
+		{2, 3, 0},
+		{8, 9, 1},
+		{7, 0, 5}
+	 };
+	 //더하기 대상 행렬(2)
+	 int B[ROWS][COLS]=
+	 {
+	 	{1, 0, 0},
+	 	{0, 1, 0},
+	 	{0, 0, 1}
+	  };
+	 // 덧셈 결과 행렬
+	 int C[ROWS][COLS];
+	 
+	 addMatrix(A, B, C);
+	 printMatrix(C); 
+
 	return 0;
 }
+
+
